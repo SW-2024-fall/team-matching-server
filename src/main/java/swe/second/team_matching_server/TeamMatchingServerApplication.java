8ab9e4f19536d3262feef9f4f320b64a2238925d
김혜주 <sueelly@naver.com>
2024-10-06 01:55:47 +0900

    init: aws s3, flyway, lombok, postgresql, spring security, jakarta, hibernate, mapstruct, jwt

---
+++ src/main/java/swe/second/team_matching_server/TeamMatchingServerApplication.java
@@ -0,0 +1,13 @@
+package swe.second.team_matching_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMatchingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamMatchingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swe/second/team_matching_server/TeamMatchingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package swe.second.team_matching_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMatchingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
