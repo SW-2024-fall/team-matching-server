55960b919ec5f8b2ae89420f3a567dd9742bbd8e
김혜주 <sueelly@naver.com>
2024-10-11 22:55:03 +0900

    Fix: File Mapper Multipart File 속성 직접 Mapping

--- src/main/java/swe/second/team_matching_server/domain/file/model/mapper/FileMapper.java
+++ src/main/java/swe/second/team_matching_server/domain/file/model/mapper/FileMapper.java
@@ -5,10 +5,16 @@ import swe.second.team_matching_server.domain.file.model.entity.File;
 import swe.second.team_matching_server.domain.file.model.dto.FileResponse;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface FileMapper {
-  File toEntity(FileCreateDto fileCreateDto);
+  @Mapping(target = "id", expression = "java(id)")
+  @Mapping(target = "url", expression = "java(url)")
+  @Mapping(target = "size", expression = "java(fileCreateDto.getFile().getSize())")
+  @Mapping(target = "originalName", expression = "java(fileCreateDto.getFile().getOriginalFilename())")
+  @Mapping(target = "mimeType", expression = "java(fileCreateDto.getFile().getContentType())")
+  File toEntity(FileCreateDto fileCreateDto, String id, String url);
 
   FileResponse toDto(File file);
 }
