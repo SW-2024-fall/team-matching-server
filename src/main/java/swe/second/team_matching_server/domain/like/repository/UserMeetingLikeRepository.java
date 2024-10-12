ca5c66b0b1f2557e28b20dee067d382777dcff74
김혜주 <sueelly@naver.com>
2024-10-12 21:50:28 +0900

    Feat: like, scrap domain 분리 & create repository

---
+++ src/main/java/swe/second/team_matching_server/domain/like/repository/UserMeetingLikeRepository.java
@@ -0,0 +1,25 @@
+package swe.second.team_matching_server.domain.like.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import swe.second.team_matching_server.domain.like.model.entity.UserMeetingLike;
+import swe.second.team_matching_server.domain.meeting.model.entity.Meeting;
+import swe.second.team_matching_server.domain.user.model.entity.User;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+@Repository
+public interface UserMeetingLikeRepository extends JpaRepository<UserMeetingLike, Long>{
+  @Query("SELECT ums.meeting FROM UserMeetingLike ums WHERE ums.user.id = :userId")
+  List<Meeting> findLikedMeetingsByUserId(@Param("userId") String userId);
+
+  @Query("SELECT ums.user FROM UserMeetingLike ums WHERE ums.meeting.id = :meetingId")
+  List<User> findLikedUsersByMeetingId(@Param("meetingId") Long meetingId);
+
+  long countByMeetingId(Long meetingId);
+
+  boolean existsByUserIdAndMeetingId(String userId, Long meetingId);
+}
--- src/main/java/swe/second/team_matching_server/domain/scrap/model/entity/UserMeetingScrap.java
+++ src/main/java/swe/second/team_matching_server/domain/scrap/model/entity/UserMeetingScrap.java
@@ -1,11 +1,12 @@
-package swe.second.team_matching_server.domain.user.model.entity;
+package swe.second.team_matching_server.domain.scrap.model.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-
 import swe.second.team_matching_server.domain.meeting.model.entity.Meeting;
+import swe.second.team_matching_server.domain.user.model.entity.User;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
@@ -14,15 +15,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.UniqueConstraint;
+import lombok.EqualsAndHashCode;
 
 @Entity
-@Table(name = "user_likes", uniqueConstraints = {
+@Table(name = "user_meeting_scraps", uniqueConstraints = {
     @UniqueConstraint(columnNames = {"user_id", "meeting_id"})
 })
 @Getter
 @AllArgsConstructor
 @Builder
-public class UserLike {
+@EqualsAndHashCode(of = {"id"})
+public class UserMeetingScrap {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
