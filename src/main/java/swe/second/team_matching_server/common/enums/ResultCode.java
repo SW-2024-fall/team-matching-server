d47979dd259e0db5409e179e8a62ae859a897ba8
김혜주 <sueelly@naver.com>
2024-10-30 02:19:10 +0900

    feat: create meeting ended & full exception case & classes

--- src/main/java/swe/second/team_matching_server/common/enums/ResultCode.java
+++ src/main/java/swe/second/team_matching_server/common/enums/ResultCode.java
@@ -13,7 +13,9 @@ public enum ResultCode {
   MEETING_NOT_FOUND("해당 모임이 존재하지 않습니다."),  
   MEETING_NO_PERMISSION("해당 작업을 수행할 권한이 없습니다."),
   MEETING_MEMBER_NOT_FOUND("해당 멤버가 존재하지 않습니다."),
-  MEETING_REQUEST_NOT_FOUND("해당 신청이 존재하지 않습니다");
+  MEETING_REQUEST_NOT_FOUND("해당 신청이 존재하지 않습니다"),
+  MEETING_ENDED("모임이 종료되었습니다."),
+  MEETING_FULL("모임이 가득 찼습니다.");
 
   private final String message;
 
