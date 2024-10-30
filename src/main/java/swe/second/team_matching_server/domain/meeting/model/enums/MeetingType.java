cf92df64d3ace1db72bcae6d01c6684742989476
김혜주 <sueelly@naver.com>
2024-10-31 03:59:45 +0900

    feat: throw exception when doesn't match enum value

--- src/main/java/swe/second/team_matching_server/domain/meeting/model/enums/MeetingType.java
+++ src/main/java/swe/second/team_matching_server/domain/meeting/model/enums/MeetingType.java
@@ -1,5 +1,7 @@
 package swe.second.team_matching_server.domain.meeting.model.enums;
 
+import swe.second.team_matching_server.domain.meeting.model.exception.MeetingInvalidMeetingTypeException;
+
 public enum MeetingType {
   REGULAR("regular"),
   ONE_TIME("one_time");
@@ -8,6 +10,14 @@ public enum MeetingType {
 
   MeetingType(String type) {
     this.type = type;
+  }
+
+  public static MeetingType from(String value) {
+    try {
+        return MeetingType.valueOf(value);
+    } catch (IllegalArgumentException e) {
+        throw new MeetingInvalidMeetingTypeException();
+    }
   }
 
   public String getType() {
