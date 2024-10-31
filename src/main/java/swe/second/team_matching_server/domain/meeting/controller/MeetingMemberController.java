b54f593ed6badc99620f81d7b52efc95bd2c5678
김혜주 <sueelly@naver.com>
2024-11-01 08:00:43 +0900

    feat(MeetingMember): member role 관리 (수정, 생성, 신청/해제/수락/거절)

--- src/main/java/swe/second/team_matching_server/domain/meeting/controller/MeetingMemberController.java
+++ src/main/java/swe/second/team_matching_server/domain/meeting/controller/MeetingMemberController.java
@@ -3,7 +3,9 @@ package swe.second.team_matching_server.domain.meeting.controller;
 import swe.second.team_matching_server.domain.meeting.service.MeetingFacadeService;
 import swe.second.team_matching_server.common.dto.ApiResponse;
 import swe.second.team_matching_server.domain.meeting.model.dto.MeetingMembers;
+import swe.second.team_matching_server.domain.meeting.model.dto.MeetingMemberUpdateDto;
 import swe.second.team_matching_server.domain.meeting.model.enums.MeetingMemberRole;
+import swe.second.team_matching_server.domain.meeting.model.dto.MeetingMemberDto;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,11 +44,31 @@ public class MeetingMemberController {
         return ApiResponse.success();
     }
 
-    // @GetMapping("/my-role")
-    // public ApiResponse<MeetingMemberRole> getMyRole(@PathVariable Long meetingId) {
-    //     // TODO: 로그인 구현 후 수정
-    //     String userId = "test";
+    @GetMapping("/my-role")
+    public ApiResponse<MeetingMemberRole> getMyRole(@PathVariable Long meetingId) {
+        // TODO: 로그인 구현 후 수정
+        String userId = "test";
 
-    //     return ApiResponse.success(meetingFacadeService.getRoleByMeetingIdAndUserId(meetingId, userId));
-    // }
+        return ApiResponse.success(meetingFacadeService.getRoleByMeetingIdAndUserId(meetingId, userId));
+    }
+
+    @PutMapping("/role")
+    public ApiResponse<MeetingMembers> updateRole(@PathVariable Long meetingId, @RequestBody MeetingMemberUpdateDto dto) {
+        // TODO: 로그인 구현 후 수정
+        String userId = "test";
+
+        MeetingMembers meetingMembers = meetingFacadeService.updateRole(userId, meetingId, dto.getTargetUserId(), dto.getRole());
+        return ApiResponse.success(meetingMembers);
+    }
+
+    @DeleteMapping
+    public ApiResponse<MeetingMembers> leave(@PathVariable Long meetingId, @RequestBody(required = false) MeetingMemberDto dto) {
+        // TODO: 로그인 구현 후 수정. 본인에 대한 것만 가능
+        String userId = "test2";
+
+        if (dto == null) {
+            return ApiResponse.success(meetingFacadeService.leave(meetingId, userId, userId));
+        }
+        return ApiResponse.success(meetingFacadeService.leave(meetingId, userId, dto.getUserId()));
+    }
 }
