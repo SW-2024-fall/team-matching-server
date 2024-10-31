eca6a29e5fc1f47c00b47c6b84bb27657a45f541
김혜주 <sueelly@naver.com>
2024-10-31 21:57:52 +0900

    feat(FileMeetingService): meeting 관련 파일들 저장, 업데이트, 삭제 메서드

--- src/main/java/swe/second/team_matching_server/domain/file/service/FileMeetingService.java
+++ src/main/java/swe/second/team_matching_server/domain/file/service/FileMeetingService.java
@@ -5,12 +5,16 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.ArrayList;
+
 import swe.second.team_matching_server.domain.file.repository.FileRepository;
 import swe.second.team_matching_server.domain.file.model.dto.FileCreateDto;
 import swe.second.team_matching_server.domain.file.model.entity.File;
 import swe.second.team_matching_server.common.enums.FileFolder;
+import swe.second.team_matching_server.domain.meeting.model.entity.Meeting;
 
 @Service
+@Transactional(readOnly = true)
 public class FileMeetingService {
   private final FileRepository fileRepository;
   private final FileService fileService;
@@ -19,21 +23,43 @@ public class FileMeetingService {
     this.fileRepository = fileRepository;
     this.fileService = fileService;
   }
-  
-  @Transactional(readOnly = true)
-  public List<File> findFilesByMeetingId(Long meetingId) {
+
+  public List<File> findAllByMeetingId(Long meetingId) {
     return fileRepository.findAllByMeetingId(meetingId);
   }
 
   @Transactional
-  public List<File> updateFilesByMeetingId(Long meetingId, List<FileCreateDto> newFileCreateDtos, List<String> deletedFileIds) {
-    List<FileCreateDto> updatedFileCreateDtos = newFileCreateDtos.stream()
-        .map(fileCreateDto -> {fileCreateDto.setFolder(FileFolder.MEETING); return fileCreateDto;})
+  public List<File> saveAllByMeeting(Meeting meeting, List<FileCreateDto> fileCreateDtos) {
+    if (fileCreateDtos == null) {
+      return new ArrayList<>();
+    }
+
+    List<FileCreateDto> updatedFileCreateDtos = fileCreateDtos.stream()
+        .map(fileCreateDto -> {
+          fileCreateDto.setFolder(FileFolder.MEETING); 
+          fileCreateDto.setMeeting(meeting); 
+          return fileCreateDto;})
+        .collect(Collectors.toList());
+    return fileService.saveAll(updatedFileCreateDtos);
+  }
+
+  @Transactional
+  public List<File> updateAllByMeeting(Meeting meeting, List<FileCreateDto> newFileCreateDtos, List<String> deletedFileIds) {
+    if (newFileCreateDtos != null) {
+      List<FileCreateDto> updatedFileCreateDtos = newFileCreateDtos.stream()
+          .map(fileCreateDto -> {
+            fileCreateDto.setFolder(FileFolder.MEETING); 
+            fileCreateDto.setMeeting(meeting); 
+            return fileCreateDto;})
         .collect(Collectors.toList());
     
-    fileService.saveFiles(updatedFileCreateDtos);
-    fileService.deleteFiles(deletedFileIds);
+      fileService.saveAll(updatedFileCreateDtos);
+    }
 
-    return findFilesByMeetingId(meetingId);
+    if (deletedFileIds != null) {
+      fileService.deleteAll(deletedFileIds);
+    }
+
+    return findAllByMeetingId(meeting.getId());
   }
 }
