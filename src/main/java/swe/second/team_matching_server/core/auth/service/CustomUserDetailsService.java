8d79bc9debb3a0376ecea8539055704ca12dc480
Chaehee03 <sch003322@gmail.com>
2024-11-08 03:01:03 +0900

    feat: CustomUserDetailsService 개발

---
+++ src/main/java/swe/second/team_matching_server/core/auth/service/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package swe.second.team_matching_server.core.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import swe.second.team_matching_server.domain.user.model.entity.User;
+import swe.second.team_matching_server.domain.user.repository.UserRepository;
+import java.util.Collections;
+
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+    }
+
+    private UserDetails createUserDetails(User user) {
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),  // 이메일을 아이디로 사용
+                user.getPassword(), // 비밀번호 사용
+                Collections.emptyList()
+        );
+    }
+}
