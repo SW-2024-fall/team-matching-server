a159609c7d2da63ec92a6d6c1ceb4a3eda41c9c8
Chaehee03 <sch003322@gmail.com>
2024-11-14 00:05:25 +0900

    feat: TokenDto 수정

--- src/main/java/swe/second/team_matching_server/core/auth/dto/TokenDto.java
+++ src/main/java/swe/second/team_matching_server/core/auth/dto/TokenDto.java
@@ -1,17 +1,24 @@
 package swe.second.team_matching_server.core.auth.dto;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@Builder
 public class TokenDto {
+    @Builder.Default
+    private String grantType = "ROLE_USER";
     private String accessToken;
     private String refreshToken;
+    private Long accessTokenExpiresIn;
+    @Builder.Default
     private String tokenType = "Bearer";  // 기본값은 "Bearer"로 설정
 
-    public TokenDto(String accessToken, String refreshToken) {
+    public TokenDto(String grantType, String accessToken, String refreshToken, Long accessTokenExpiresIn, String tokenType) {
+        this.grantType = grantType;
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
+        this.accessTokenExpiresIn = accessTokenExpiresIn;
+        this.tokenType = tokenType;
     }
 }
