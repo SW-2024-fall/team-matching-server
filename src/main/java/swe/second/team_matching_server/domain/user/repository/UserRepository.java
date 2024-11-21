8948d35dfaea8a5a991347ced324c67f27b62042
Chaehee03 <sch003322@gmail.com>
2024-11-22 00:45:52 +0900

    feat: UserRepository에 findById 추가

--- src/main/java/swe/second/team_matching_server/domain/user/repository/UserRepository.java
+++ src/main/java/swe/second/team_matching_server/domain/user/repository/UserRepository.java
@@ -1,14 +1,17 @@
 package swe.second.team_matching_server.domain.user.repository;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import swe.second.team_matching_server.domain.user.model.entity.User;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    @NotNull Optional<User> findById(@NotNull String id);
 }
