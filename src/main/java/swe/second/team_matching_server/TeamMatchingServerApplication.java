f123edfe37962809301d531900f2b772f64c2737
Chaehee03 <sch003322@gmail.com>
2024-11-22 00:44:47 +0900

    fix: TeamMatchingServerApplication 어노테이션 추가

--- src/main/java/swe/second/team_matching_server/TeamMatchingServerApplication.java
+++ src/main/java/swe/second/team_matching_server/TeamMatchingServerApplication.java
@@ -2,11 +2,16 @@ package swe.second.team_matching_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"swe.second.team_matching_server.domain.file.repository"})
 public class TeamMatchingServerApplication {
 
+	
 	public static void main(String[] args) {
+		System.setProperty("slf4j.internal.verbosity", "WARN");
 		SpringApplication.run(TeamMatchingServerApplication.class, args);
 	}
 
