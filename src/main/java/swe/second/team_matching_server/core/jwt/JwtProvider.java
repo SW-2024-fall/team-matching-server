b6d7e042862dc36cee50afdaf2b194df998e6696
김혜주 <sueelly@naver.com>
2024-11-26 02:30:00 +0900

    feat(JwtProvider): create JwTProvider(create/validate/getUserId)

---
+++ src/main/java/swe/second/team_matching_server/core/jwt/JwtProvider.java
@@ -0,0 +1,84 @@
+package swe.second.team_matching_server.core.jwt;
+
+import java.util.Date;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import swe.second.team_matching_server.domain.auth.model.dto.TokenResponse;
+import swe.second.team_matching_server.domain.auth.model.exception.ExpiredTokenException;
+import swe.second.team_matching_server.domain.auth.model.exception.IllegalArgumentToken;
+import swe.second.team_matching_server.domain.auth.model.exception.InvalidSignatureException;
+import swe.second.team_matching_server.domain.auth.model.exception.InvalidTokenException;
+import swe.second.team_matching_server.domain.auth.model.exception.UnsupportedTokenException;
+
+@Component
+public class JwtProvider {
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.access-token-expiration}")
+    private Long accessTokenExpiration;
+
+    @Value("${jwt.refresh-token-expiration}")
+    private Long refreshTokenExpiration;
+
+    public TokenResponse createToken(String userId) {
+        return new TokenResponse(createAccessToken(userId), createRefreshToken(userId));
+    }
+
+    public String createAccessToken(String userId) {
+        return Jwts.builder()
+            .setSubject(userId)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpiration))
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact();
+    }
+
+    public String createRefreshToken(String userId) {
+        return Jwts.builder()
+            .setSubject(userId)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpiration))
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact();
+    }
+
+    public String getUserId(String token) {
+        return Jwts.parser()
+            .setSigningKey(secretKey)
+            .parseClaimsJws(token)
+            .getBody()
+            .getSubject();
+    }
+
+    public Authentication getAuthentication(String token) {
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+
+            return true;
+        } catch (ExpiredJwtException e) {
+            throw new ExpiredTokenException();
+        } catch (io.jsonwebtoken.MalformedJwtException e) {
+            throw new IllegalArgumentToken();
+        } catch (io.jsonwebtoken.SignatureException e) {
+            throw new InvalidSignatureException();
+        } catch (io.jsonwebtoken.UnsupportedJwtException e) {
+            throw new UnsupportedTokenException();
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentToken();
+        } catch (Exception e) {
+            throw new InvalidTokenException();
+        }
+    }
+}
