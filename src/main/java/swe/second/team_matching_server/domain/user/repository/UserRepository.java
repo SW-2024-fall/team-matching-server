99a2569a07284bf1f34d0bbe3535220df0c9775a
김혜주 <sueelly@naver.com>
2024-11-29 10:28:45 +0900

    fix(Meeting): Fix not to get deleted users

--- src/main/java/swe/second/team_matching_server/domain/user/repository/UserRepository.java
+++ src/main/java/swe/second/team_matching_server/domain/user/repository/UserRepository.java
@@ -3,11 +3,16 @@ package swe.second.team_matching_server.domain.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import swe.second.team_matching_server.domain.user.model.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.deletedAt is null")
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.id = :userId AND u.deletedAt is null")
+    Optional<User> findById(String userId);
 }
